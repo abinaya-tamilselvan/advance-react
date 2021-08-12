19e0279fad3892c0cf07a809fcf13cb9c00bc234
abinaya-tamilselvan <abinaya.tamilselvan@soprasteria.com>
2021-08-12 22:37:21 +0530

    UseStateCounter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 //import Index from "./tutorial/Reducer/index"
 //import LearnJSON from "./tutorial/LearnJSON";
 //import  UseStateErrorExample from './tutorial/usestateErrorExample';
-import UseStateObject from './tutorial/UseStateObject';
-
+//import UseStateObject from './tutorial/UseStateObject';
+import UseStateCounter from './tutorial/useStateCounter'
 
 
 function App() {
   return (
      <div className ='container'>
      {/* <UseStateErrorExample /> */}
-      <UseStateObject />
+     {/* <UseStateObject /> */}
+     <UseStateCounter />
      
      </div>
   );
